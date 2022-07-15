d5a7c20f1dde5ea6db678b393bd8ab6f0c5f5524
assafrbi <73109803+assafrbi@users.noreply.github.com>
2022-07-15 11:49:13 +0300

    Update min.py

--- min.py
+++ min.py
@@ -13,7 +13,9 @@ from imdb import Cinemagoer
 import tqdm
 import time
 import nltk
-nltk.download('stopwords')
+# nltk.download('stopwords')
+nltk.download('all')
+# nltk.download('punkt')
 # Gensim
 import gensim
 import gensim.corpora as corpora
@@ -38,134 +40,134 @@ import swifter
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 # Maximum number of threads that will be spawned
-MAX_THREADS = 6
+# MAX_THREADS = 6
 
-movie_title_arr = []
-movie_year_arr = []
-movie_genre_arr = []
-image_id_arr = []
-movie_synopsis_arr = []
-need_wiki_id = []
-need_wiki_name = []
-need_wiki_year = []
-
-
-ia = Cinemagoer()
-
-def getSynopsisFromImdb(id):
-  movie = ia.get_movie(id)
-  print("for id {}\n".format(id))
-  print("\n summary is :\n {}\n".format(movie.get('plot')[0]))
-  return movie.get('plot')[0]
+# movie_title_arr = []
+# movie_year_arr = []
+# movie_genre_arr = []
+# image_id_arr = []
+# movie_synopsis_arr = []
+# need_wiki_id = []
+# need_wiki_name = []
+# need_wiki_year = []
 
 
-def getMovieTitle(header):
-    try:
-        return header[0].find("a").getText()
-    except:
-        return 'NA'
+# ia = Cinemagoer()
 
-def getReleaseYear(header):
-    try:
-        return header[0].find("span",  {"class": "lister-item-year text-muted unbold"}).getText().strip()[-5:-1]
-    except:
-        return 'NA'
+# def getSynopsisFromImdb(id):
+#   movie = ia.get_movie(id)
+#   print("for id {}\n".format(id))
+#   print("\n summary is :\n {}\n".format(movie.get('plot')[0]))
+#   return movie.get('plot')[0]
 
-def getGenre(muted_text):
-    try:
-        return muted_text.find("span",  {"class":  "genre"}).getText().strip()
-    except:
-        return 'NA'
+
+# def getMovieTitle(header):
+#     try:
+#         return header[0].find("a").getText()
+#     except:
+#         return 'NA'
+
+# def getReleaseYear(header):
+#     try:
+#         return header[0].find("span",  {"class": "lister-item-year text-muted unbold"}).getText().strip()[-5:-1]
+#     except:
+#         return 'NA'
+
+# def getGenre(muted_text):
+#     try:
+#         return muted_text.find("span",  {"class":  "genre"}).getText().strip()
+#     except:
+#         return 'NA'
         
-def getsynopsys(movie, id, name, year):
-    try:
-        movie_synopsis = getSynopsisFromImdb(id)
-        return movie_synopsis
-    except:
-      # no imdb data -> need wiki
-      need_wiki_id.append(id)
-      need_wiki_name.append(name)
-      need_wiki_year.append(year)
-      return 'NA'
+# def getsynopsys(movie, id, name, year):
+#     try:
+#         movie_synopsis = getSynopsisFromImdb(id)
+#         return movie_synopsis
+#     except:
+#       # no imdb data -> need wiki
+#       need_wiki_id.append(id)
+#       need_wiki_name.append(name)
+#       need_wiki_year.append(year)
+#       return 'NA'
 
-def getImageId(image):
-    try:
-        return image.get('data-tconst')[2:]
-    except:
-        return 'NA'
+# def getImageId(image):
+#     try:
+#         return image.get('data-tconst')[2:]
+#     except:
+#         return 'NA'
 
 
-def main(imdb_url):
-    response = requests.get(imdb_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+# def main(imdb_url):
+#     response = requests.get(imdb_url)
+#     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Movie Name
-    movies_list  = soup.find_all("div", {"class": "lister-item mode-advanced"})
+#     # Movie Name
+#     movies_list  = soup.find_all("div", {"class": "lister-item mode-advanced"})
     
-    for indx, movie in enumerate(movies_list):
-        header = movie.find_all("h3", {"class":  "lister-item-header"})
-        muted_text = movie.find_all("p", {"class":  "text-muted"})[0]
-        imageDiv =  movie.find("div", {"class": "lister-item-image float-left"})
-        image = imageDiv.find("img", "loadlate")
+#     for indx, movie in enumerate(movies_list):
+#         header = movie.find_all("h3", {"class":  "lister-item-header"})
+#         muted_text = movie.find_all("p", {"class":  "text-muted"})[0]
+#         imageDiv =  movie.find("div", {"class": "lister-item-image float-left"})
+#         image = imageDiv.find("img", "loadlate")
         
-        # Movie Title
-        movie_title = getMovieTitle(header)
-        movie_title_arr.append(movie_title)
+#         # Movie Title
+#         movie_title = getMovieTitle(header)
+#         movie_title_arr.append(movie_title)
 
-        # Movie id
-        id = getImageId(image)
-        image_id_arr.append(id)
+#         # Movie id
+#         id = getImageId(image)
+#         image_id_arr.append(id)
         
-        # Movie release year
-        year = getReleaseYear(header)
-        movie_year_arr.append(year)
+#         # Movie release year
+#         year = getReleaseYear(header)
+#         movie_year_arr.append(year)
 
-        # Movie Synopsys
-        synopsis = getsynopsys(movie, id, movie_title, year)
-        movie_synopsis_arr.append(synopsis)
+#         # Movie Synopsys
+#         synopsis = getsynopsys(movie, id, movie_title, year)
+#         movie_synopsis_arr.append(synopsis)
         
-        # Genre
-        genre = getGenre(muted_text)
-        movie_genre_arr.append(genre)
+#         # Genre
+#         genre = getGenre(muted_text)
+#         movie_genre_arr.append(genre)
         
-# An array to store all the URL that are being queried
-imageArr = []
+# # An array to store all the URL that are being queried
+# imageArr = []
 
-# Maximum number of pages one wants to iterate over
-MAX_PAGE =51
+# # Maximum number of pages one wants to iterate over
+# MAX_PAGE =51
 
-# Loop to generate all the URLS.
-for i in range(1, 1500, 50):
-    imdb_url = f'https://www.imdb.com/search/title/?title_type=feature&countries=il&start={i}&ref_=adv_nxt'
-    imageArr.append(imdb_url)
+# # Loop to generate all the URLS.
+# for i in range(1, 1500, 50):
+#     imdb_url = f'https://www.imdb.com/search/title/?title_type=feature&countries=il&start={i}&ref_=adv_nxt'
+#     imageArr.append(imdb_url)
 
 
-def download_stories(story_urls):
-    threads = min(MAX_THREADS, len(story_urls))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        time.sleep(0.3)
-        executor.map(main, story_urls)
+# def download_stories(story_urls):
+#     threads = min(MAX_THREADS, len(story_urls))
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+#         time.sleep(0.3)
+#         executor.map(main, story_urls)
 
-# Call the download function with the array of URLS called imageArr
-download_stories(imageArr)
+# # Call the download function with the array of URLS called imageArr
+# download_stories(imageArr)
 
-# Attach all the data to the pandas dataframe. You can optionally write it to a CSV file as well
-movieDf = pd.DataFrame({
-    "Title": movie_title_arr,
-    "Year": movie_year_arr,
-    "Genre": movie_genre_arr,
-    "Synopsis": movie_synopsis_arr,
-    "IMDb id": image_id_arr,
-})
+# # Attach all the data to the pandas dataframe. You can optionally write it to a CSV file as well
+# movieDf = pd.DataFrame({
+#     "Title": movie_title_arr,
+#     "Year": movie_year_arr,
+#     "Genre": movie_genre_arr,
+#     "Synopsis": movie_synopsis_arr,
+#     "IMDb id": image_id_arr,
+# })
 
-needWiki = pd.DataFrame({
-    "Title": need_wiki_name,
-    "Year": need_wiki_year,
-    "IMDb id": need_wiki_id,
-})
+# needWiki = pd.DataFrame({
+#     "Title": need_wiki_name,
+#     "Year": need_wiki_year,
+#     "IMDb id": need_wiki_id,
+# })
 
-movieDf.to_csv('israel_originated_movies_imdb.csv')
-needWiki.to_csv('movies_need_wiki.csv')
+# movieDf.to_csv('israel_originated_movies_imdb.csv')
+# needWiki.to_csv('movies_need_wiki.csv')
 
 
 
@@ -174,13 +176,13 @@ needWiki.to_csv('movies_need_wiki.csv')
 
 summary_genre = ["Synopsis","Genre"]
 data = pd.read_csv('israel_originated_movies_imdb.csv', usecols=summary_genre)
-print(data["Genre"].iloc[1])
-print("\n{}".format(data["Synopsis"].iloc[1]))
+# print(data["Genre"].iloc[1])
+# print("\n{}".format(data["Synopsis"].iloc[1]))
 stopwords = stopwords.words("english")
 # print (stopwords)
-print(data.isnull().sum())
+# print(data.isnull().sum())
 data = data.dropna()
-print(data.isnull().sum())
+# print(data.isnull().sum())
 
 
 data.to_csv('ready_for_topic_modeling.csv', index=False)
@@ -206,6 +208,7 @@ def remove_verbs(text):
     final_text = ''
     allowedWordTypes = ["J","R","N"]
     tokens = nltk.word_tokenize(text)
+    print(tokens)
     tagged = nltk.pos_tag(tokens)
     for w in tagged:
         if w[1][0] in allowedWordTypes:
@@ -314,16 +317,21 @@ def create_topic_specific_wordcloud(ready_data, source, topic):
     return wordcloud.to_image()
 
 
-    ###### PRE-PROCESS #######
+###### PRE-PROCESS #######
 
-
+ready_data.loc[ready_data['Synopsis']!='']
 source = 'Synopsis'
 data = ready_data.copy()
+
 print(f'---Doing pre-processing for {source}..')
 print('Initial pre-processing..')
-data[source] = data[source].swifter.apply(lambda x: text_preprocess(x))
+
+
+data[source] = data[source].apply(lambda x: text_preprocess(x))
+
 data = data[source].to_list()
 data_words = list(sent_to_words(data))
+
 
 # Build the bigram model
 bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100, progress_per=200) # higher threshold fewer phrases.
@@ -338,9 +346,11 @@ data_words_nostops = remove_stopwords(data_words)
 # Form Bigrams
 print('Creating bigrams..')
 ready_data[f'{source}_words_bigrams'] = make_bigrams(data_words_nostops)
-
+print('stage 2 done')
 # Initialize spacy 'en' model, keeping only tagger component (for efficiency)
 # python -m spacy download en_core_web_sm
+
+
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 
 # Do lemmatization keeping only noun, adj, vb, adv
@@ -352,7 +362,7 @@ ready_data = ready_data.drop(columns=[f'{source}_words_bigrams'])
 ready_data[f'{source}_preprocessed'] = ready_data[f'{source}_lemmatized'].apply(lambda x: join_list(x))
 
 del bigram, bigram_mod, data_words, data, nlp
-
+print('stage 3 done')
 
 
 
@@ -367,7 +377,7 @@ long_string = ','.join(ready_data[f'{source}_preprocessed'].to_list())
 wordcloud.generate(long_string)
 # Visualize the word cloud
 wordcloud.to_image()
-
+print('stage 4 done')
 
 
 
@@ -378,7 +388,7 @@ num_topics = 6
 lda_model, corpus, id2word = build_LDA_model(ready_data, source=source, num_topics=num_topics)
 
 lda_model.print_topics()
-
+print('stage 5 done')
 
 # Compute Perplexity
 print('\nPerplexity: ', lda_model.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
